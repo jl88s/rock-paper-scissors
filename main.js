38297e2ffc6ae5f23ce53056f9f70d12d483e7d9
Jomaree Lawsin <jclawsin88@gmail.com>
2019-04-17 11:05:46 -0700

    Revamped half of the code base in order to manipulate the DOM

--- main.js
+++ main.js
@@ -1,89 +1,104 @@
 
-/// 0... Create a Human and Computer variables to store their scores
+/// 0... Create a Human and Computer variables to store their scores & DOM id's
 let playerScore = 0;
 let computerScore = 0;
-let rounds = 0;
 
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const resetBtn = document.getElementById("reset-icon");
 
 // 1... Computer randomly selects from the array and returns a value.
 function computerPlay() {
-     let computerRandomChoices = ['rock', 'paper', 'scissors'];
-     let getRandomChoices = Math.floor(Math.random() * computerRandomChoices.length)
+    let computerRandomChoices = ['rock', 'paper', 'scissors'];
+    let getRandomChoices = Math.floor(Math.random() * computerRandomChoices.length)
     return computerRandomChoices[getRandomChoices];
 }
 
-// 2... Player inputs their selection and returns a case-insensitive value.
-function humanPlay() {
-    let playerInput = prompt("Please select one: Rock, Paper, or Scissors?");
-
-    // Checks if user input is not empty.
-    if (playerInput === null) {
-        return "null";
-    } else if (playerInput === "") {
-        alert("You can't leave the box empty!")
-    } else {
-        // this took me a day and a half to fingure out!
-       return playerInput.toLowerCase();
-    }
-}
-
 // 3... Make a function that plays a single round of Rock, Paper, Scissors.
-function playRound() {
-    let playerSelection = humanPlay();
-    let computerSelection = computerPlay();
+function playRound(playerSelection, computerSelection) {
+    console.log(`You selected: "${playerSelection}"`);
+    console.log(`Computer selected: "${computerSelection}"`);
 
     // Tie 
     if (playerSelection === computerSelection) {
-        rounds * 1;
-        return "It's a tie! Play again!";
+        document.getElementById('game-result').innerHTML = "It's a tie! Play again!";
+        document.getElementById('game-result').style.color = "black";
     }
-
     // Rock beats Scissors
-    if (playerSelection === 'rock') {
+    else if (playerSelection === 'rock') {
         if (computerSelection === 'scissors') {
             playerScore++;
-            return "You win! Rock beats Scissors!";
+            document.getElementById('game-result').innerHTML = "You win! Rock beats Scissors!";
+            document.getElementById('game-result').style.color = "green";
+            document.getElementById('human-score').innerHTML = playerScore;
         } else {
             computerScore++;
-            return "You lose, Paper beats Rock!";
+            document.getElementById('game-result').innerHTML = "You lose! Paper beats Rock!";
+            document.getElementById('game-result').style.color = "red";
+            document.getElementById('robot-score').innerHTML = computerScore;
         }
-    }  
+    }
     // Paper beats Rock
     else if (playerSelection === 'paper') {
         if (computerSelection === 'rock') {
             playerScore++;
-            return "You win! Paper beats Rock!"
+            document.getElementById('game-result').innerHTML = "You win! Paper beats Rock!"
+            document.getElementById('game-result').style.color = "green";
+            document.getElementById('human-score').innerHTML = playerScore;
         } else {
             computerScore++;
-            return "You lose, Scissors beats Paper!";
+            document.getElementById('game-result').innerHTML = "You lose! Scissors beats Paper!";
+            document.getElementById('game-result').style.color = "red";
+            document.getElementById('robot-score').innerHTML = computerScore;
         }
-    }   
+    }
     // Scissors beat Paper
     else if (playerSelection === 'scissors') {
         if (computerSelection === 'paper') {
             playerScore++;
-            return "You win! Scissors beats Paper!";
+            document.getElementById('game-result').innerHTML = "You win! Scissors beats Paper!";
+            document.getElementById('game-result').style.color = "green";
+            document.getElementById('human-score').innerHTML = playerScore;
         } else {
             computerScore++;
-            return "You lose, Rock beats Scissors!";
+            document.getElementById('game-result').innerHTML = "You lose! Rock beats Scissors!";
+            document.getElementById('game-result').style.color = "red";
+            document.getElementById('robot-score').innerHTML = computerScore;
         }
     }
 }
- 
+
 //  4... Play a 5 round game that keeps score
-function game() {
-    for (let rounds = 1 ; rounds <= 5 ; rounds++) {
-        console.log(playRound());
-        console.log(`Round ${rounds} [ Your score: ${playerScore} | Computer's score: ${computerScore} ]`);
-      }
+function game(playerChoice) {
+    let player = playerChoice;
+    let computer = computerPlay();
 
-      if (playerScore == 5) {
-        return "CONGRATULATIONS - YOU WON!";
+    playRound(player, computer);
+
+    if (playerScore == 5) {
+        document.getElementById('computer-play-result').innerHTML = "WINNER WINNER CHICKEN DINNER!!!";
+        document.getElementById('computer-play-result').style.color = "green";
+        document.getElementById('computer-play-result').style.fontSize = "1.8em";
+        document.getElementById('game-result').innerHTML = "";
     } else if (computerScore == 5) {
-        return "THE COMPUTER HAS DEFEATED YOU! YOU LOSE!";
-    } else {
-        return "It's a draw!";
+        document.getElementById('computer-play-result').innerHTML = "The computer has defeated you!";
+        document.getElementById('computer-play-result').style.color = "red";
+        document.getElementById('computer-play-result').style.fontSize = "1.8em";
+        document.getElementById('game-result').innerHTML = "";
     }
-}
+};
 
-game();
+//  5.... On click of event listener execute game function after user chooses RPS.
+rock.addEventListener('click', () => {
+    game("rock");
+});
+paper.addEventListener('click', () => {
+    game("paper");
+});
+scissors.addEventListener('click', () => {
+    game("scissors");
+});
+resetBtn.addEventListener('click', function () {
+    location.reload();
+});
